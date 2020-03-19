3e03e9eeb8d7494737288194fe1c1abf60cf112d
spikeygriffiths <jonathan@rosepip.com>
2020-03-19 22:15:16 +0000

    Added power meter gfx

--- Power.ino
+++ Power.ino
@@ -6,6 +6,10 @@ int oldPowerDetail;
 void RenderPower(char* report, bool reportChange)
 {
   char powerTxt[10];
+  char jpegName[40];  // jpegName must be at least 5 chars longer than icon
+  int powerVal;
+  int suffixWidth;
+
   reportChange |= CmpDictVal(report, "powerNow", powerNow); // Check to see if any of the values we care about have changed since last time
   reportChange |= CmpDictVal(report, "energyToday", energyToday);
   if (!reportChange) return;  // Exit if nothing changed
@@ -16,16 +20,42 @@ void RenderPower(char* report, bool reportChange)
     strcpy(powerTxt, powerNow);
     strcpy(powerTxt+1, ".");  // Assume just single digit of kW
     strcpy(powerTxt+2, powerNow+1);
-    strcpy(powerTxt+4, "kW"); // Lose units digit of power (otherwise string gets too long!)
+    powerTxt[4] = 0;  // Lose units
+    suffixWidth = 15; // Allow for small "kW" after digits
   } else { // 3 or fewer digits - show in W
     strcpy(powerTxt, powerNow);
-    strcat(powerTxt, "W");
+    suffixWidth = 10; // Allow for small "kW" after digits
   }
+  powerVal = atoi(powerNow);
   tft.fillScreen(TFT_WHITE);
   tft.setTextColor(TFT_BLACK, TFT_WHITE);
   tft.setTextDatum(MC_DATUM);
+  jpegName[0] = '\0';
+  strcpy(jpegName, "/Meter_");
+  if (powerVal < 500) {
+    strcat(jpegName, "33");
+  } else if (powerVal < 1000) {
+    strcat(jpegName, "67");
+  } else {
+    strcat(jpegName, "100");
+  }
+  strcat(jpegName, ".jpg");
+  Debug("Power Jpeg:"); DebugLn(jpegName);
+  fex.drawJpeg(jpegName, 15,0, nullptr);  // Draw JPEG directly to screen
   tft.loadFont("Cambria-48");   // Name of font file (library adds leading / and .vlw)
-  PrettyLine(powerTxt, 20, JUSTIFY_CENTRE);
+  //PrettyLine(powerTxt, 50, JUSTIFY_CENTRE);
+  int startX = (TFT_HEIGHT/2) - (tft.textWidth(powerTxt) / 2) - suffixWidth;  // Must calculate this while using correct font.  -suffixWidth to allow for "W" or "kW"
+  int endX = (TFT_HEIGHT/2) + (tft.textWidth(powerTxt) / 2) - suffixWidth;  // Must calculate this while using correct font
+  tft.setCursor(startX, 50);
+  tft.print(powerTxt);
+  tft.unloadFont(); // To recover RAM
+  tft.loadFont("Cambria-24");   // Name of font file (library adds leading / and .vlw)
+  tft.setCursor(endX+2, 50+24-6);
+  if (strlen(powerNow) > 3) { // Convert to kW if > 1000W
+    tft.print("kW"); // Lose units digit of power (otherwise string gets too long!)
+  } else { // 3 or fewer digits - show in W
+    tft.print("W");
+  }
   tft.unloadFont(); // To recover RAM
 }
 
